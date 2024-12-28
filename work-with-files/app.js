b2eceb293a9591d36f914d066336bfec8bcf90fb
Allovallo <grossco@meta.ua>
2024-12-28 15:58:13 +0200

    files updated

--- work-with-files/app.js
+++ work-with-files/app.js
@@ -1,12 +1,31 @@
 const fs = require("fs/promises");
+// const fs = require("fs").promises
 
-const fileOperation = async () => {
-  const data = await fs.readFile("./files/file.txt");
-  console.log(data);
+// const replaceText = async () => {
+//   const result = await fs.writeFile("./files/file.txt", "Гегель");
+//   console.log(result);
+// };
+
+// replaceText();
+
+const addText = async () => {
+  const result = await fs.appendFile("./files/file.txt", "./files/file1.txt");
+  console.log(result);
 };
 
-fileOperation();
+addText();
 
+/*
+const readFile = async () => {
+  const text = await fs.readFile("./files/file.txt", "utf-8");
+  //   const buffer = await fs.readFile("./files/file.txt");
+  //   console.log(buffer);
+  //   const text = buffer.toString();
+  console.log(text);
+};
+
+readFile();
+*/
 // fs.readFile("./files/file.txt")
 //   .then((data) => console.log(data))
 //   .catch((error) => console.log(error.message));
