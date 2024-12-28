e3e1b524afd536ad58a0b2655e45133c03602570
Allovallo <grossco@meta.ua>
2024-12-28 15:15:22 +0200

    files updated

--- node-module-es6-type/index.js
+++ node-module-es6-type/index.js
@@ -1,25 +1,4 @@
 import users from "./users.js";
-import {getCurrentMonth} from "./date/index.js";
+import { getCurrentMonth } from "./date/index.js";
 
 console.log(users);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- work-with-files/app.js
+++ work-with-files/app.js
@@ -1,37 +1,19 @@
 const fs = require("fs/promises");
-// const fs = require("fs").promises;
 
-/*
-const readFile = async() => {
-    const text = await fs.readFile("./files/file.txt", "utf-8");
-    console.log(text);
-    // const buffer = await fs.readFile("./files/file.txt");
-    // const text = buffer.toString();
-    // console.log(text)
-}
+const fileOperation = async () => {
+  const data = await fs.readFile("./files/file.txt");
+  console.log(data);
+};
 
-readFile()
-*/
-/*
-const addText = async()=> {
-    await fs.appendFile("./files/file2.txt", "\nТак говорил Заратустра");
-}
+fileOperation();
 
-addText();
-*/
-/*
-const replaceText = async()=> {
-    const result = await fs.writeFile("./files/file3.txt", "Ницше");
-    console.log(result);
-}
-
-replaceText();
-*/
 // fs.readFile("./files/file.txt")
-//     .then(data => console.log(data))
-//     .catch(error => console.log(error.message))
+//   .then((data) => console.log(data))
+//   .catch((error) => console.log(error.message));
+
+// const fs = require("fs");
 
 // fs.readFile("./files/file.txt", (error, data) => {
-//     console.log(error);
-//     console.log(data);
-// })
+//   console.log(error);
+//   console.log(data);
+// });
