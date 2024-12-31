ae8b78d68fe3c5035042be43595cd3c03f8a88db
Allovallo <grossco@meta.ua>
2024-12-31 16:30:51 +0200

    files updated

--- project-2/work-with-json-files/app.js
+++ project-2/work-with-json-files/app.js
@@ -1,3 +1,12 @@
 const books = require("./books");
 
-console.log(books);
+const invokeAction = async ({ action, id, title, author }) => {
+  switch (action) {
+    case "read":
+      const allBooks = await books.getAll();
+      return console.log(allBooks);
+  }
+};
+
+invokeAction({ action: "read" });
+// invokeAction({ action: "getById", id: "u9kgwNWGi3uUUwh0b8V49" });
--- project-2/work-with-json-files/books/index.js
+++ project-2/work-with-json-files/books/index.js
@@ -1,4 +1,13 @@
-const getAll = () => {};
+const fs = require("fs/promises");
+const path = require("path");
+
+const booksPath = path.join(__dirname, "books.json");
+// console.log(booksPath);
+
+const getAll = async () => {
+  const data = await fs.readFile(booksPath, "utf-8");
+  return JSON.parse(data);
+};
 
 module.exports = {
   getAll,
