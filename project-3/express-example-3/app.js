cf276bcd3c9bd83cbff6d421e7c18fe46bd744db
Allovallo <grossco@meta.ua>
2025-02-15 17:15:26 +0200

    files updated

--- project-3/express-example-3/app.js
+++ project-3/express-example-3/app.js
@@ -4,6 +4,16 @@ const books = require("./books");
 
 const app = express();
 
+// app.use((req, res, next) => {
+//   console.log("First middleware");
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   console.log("Second middleware");
+//   next();
+// });
+
 app.get("/products", (req, res) => {
   res.json([]);
 });
