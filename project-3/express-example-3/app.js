ac36b7a3be7efba9ff43532c5e3340b01dac5277
Allovallo <grossco@meta.ua>
2025-02-15 17:37:54 +0200

    files updated

--- project-3/express-example-3/app.js
+++ project-3/express-example-3/app.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const moment = require("moment");
 
 const books = require("./books");
 
 const app = express();
+
+app.use((req, res, next) => {
+  const { method, url } = req;
+  const date = moment().format("DD-MM-YYYY_hh:mm:ss");
+});
 
 // app.use((req, res, next) => {
 //   console.log("First middleware");
