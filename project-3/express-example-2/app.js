87631e41e57b202a5f9c0a1e5b61e4ec10e131a3
Allovallo <grossco@meta.ua>
2025-02-15 12:47:41 +0200

    files updated

--- project-3/express-example-2/app.js
+++ project-3/express-example-2/app.js
@@ -4,12 +4,11 @@ const books = require("./books");
 
 const app = express();
 
-app.get("/books", (req, res)=> {
-    const databaseResponse = null;
-    // res.json(databaseResponse);
-    // res.send(databaseResponse);
-    res.json(books);
-    // res.send(books);
-})
+app.get("/books", (req, res) => {
+  //   const databaseResponse = null;
+  //   res.json(databaseResponse);
+  res.json(books);
+  //   res.send(books);
+});
 
 app.listen(3000);
