9358cdceebc4610b221541aa8acdbe09fcfb5102
Allovallo <grossco@meta.ua>
2025-02-25 18:23:21 +0200

    files updated

--- project-3/express-example-2/app.js
+++ project-3/express-example-2/app.js
@@ -5,10 +5,7 @@ const books = require("./books");
 const app = express();
 
 app.get("/books", (req, res) => {
-  //   const databaseResponse = null;
-  //   res.json(databaseResponse);
   res.json(books);
-  //   res.send(books);
 });
 
 app.listen(3000);
--- project-3/express-example-3/app.js
+++ project-3/express-example-3/app.js
@@ -1,25 +1,12 @@
 const express = require("express");
-const moment = require("moment");
-const fs = require("fs/promises");
-const cors = require("cors");
 
 const books = require("./books");
 
 const app = express();
 
-// const corsMiddleware = cors();
-// app.use(corsMiddleware);
-
-app.use(cors());
-
-/*
-app.use(async (req, res, next) => {
+app.use((req, res, next) => {
   const { method, url } = req;
-  const date = moment().format("DD-MM-YYYY_hh:mm:ss");
-  await fs.appendFile("./public/server.log", `\n${method} ${url} ${date}`);
-  next();
 });
-*/
 
 // app.use((req, res, next) => {
 //   console.log("First middleware");
@@ -31,16 +18,12 @@ app.use(async (req, res, next) => {
 //   next();
 // });
 
-app.get("/products", async (req, res) => {
+app.get("/products", (req, res) => {
   res.json([]);
 });
 
-app.get("/books", async (req, res) => {
+app.get("/books", (req, res) => {
   res.json(books);
-});
-
-app.use((req, res) => {
-  res.status(404).json({ message: "Not found" });
 });
 
 app.listen(3000);
