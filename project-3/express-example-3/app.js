47722717a3f18637f1569df4afaa341084e7228c
Allovallo <grossco@meta.ua>
2025-02-26 16:39:20 +0200

    files updated

--- project-3/express-example-3/app.js
+++ project-3/express-example-3/app.js
@@ -1,12 +1,23 @@
 const express = require("express");
+const moment = require("moment");
+const fs = require("fs/promises");
+const cors = require("cors");
 
 const books = require("./books");
 
 const app = express();
 
-app.use((req, res, next) => {
-  const { method, url } = req;
-});
+// const corsMiddleware = cors();
+// app.use(corsMiddleware);
+
+app.use(cors());
+
+// app.use(async (req, res, next) => {
+//   const { method, url } = req;
+//   const date = moment().format("DD-MM-YYYY_hh:mm:ss");
+//   await fs.appendFile("./public/server.log", `\n${method} ${url} ${date}`);
+//   next();
+// });
 
 // app.use((req, res, next) => {
 //   console.log("First middleware");
@@ -24,6 +35,10 @@ app.get("/products", (req, res) => {
 
 app.get("/books", (req, res) => {
   res.json(books);
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found!" });
 });
 
 app.listen(3000);
