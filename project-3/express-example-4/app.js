808448a0fb8d9eb553646cd88ca2866383746697
Allovallo <grossco@meta.ua>
2025-02-27 15:58:09 +0200

    files updated

--- project-3/express-example-4/app.js
+++ project-3/express-example-4/app.js
@@ -1,30 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 
-const books = rquire("./books");
+const booksRouter = require("./routes/api/books");
 
 const app = express();
 
 app.use(cors());
 
-app.get("/api/books", (req, res) => {
-  res.json(books);
-});
-
-app.get("api/books/:id", (req, res) => {
-  res.json(books[0]);
-});
-
-app.post("/api/books", (req, res) => {
-  res.json(books[0]);
-});
-
-app.put("/api/books/:id", (req, res) => {
-  res.json(books[0]);
-});
-
-app.delete("/api/books/:id", (req, res) => {
-  res.json(books[0]);
-});
+app.use("/api/books", booksRouter);
 
 app.listen(3000);
