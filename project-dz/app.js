33b8f6871d83d07d3211a9bdfacd86c4bdf22ca9
Allovallo <grossco@meta.ua>
2025-03-02 11:42:12 +0200

    files updated

--- project-dz/app.js
+++ project-dz/app.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const cors = require("cors");
 
 const booksRouter = require("./routes/api/books");
-const contactsRouter = require("./routes/api/contacts");
+// const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
 
@@ -14,7 +14,7 @@ app.use(cors());
 // app.use(express.json());
 
 app.use("/api/books", booksRouter);
-app.use("/api/contacts", contactsRouter);
+// app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
@@ -24,4 +24,4 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
 });
 
-module.exports == app;
+module.exports = app;
