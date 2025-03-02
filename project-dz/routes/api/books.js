6ce3286f747daaaf52f6a8a3ef00013fba16081a
Allovallo <grossco@meta.ua>
2025-03-02 16:33:11 +0200

    files updated

--- project-dz/routes/api/books.js
+++ project-dz/routes/api/books.js
@@ -2,11 +2,39 @@ const express = require("express");
 
 const books = require("../../models/books");
 
+const { HttpError } = require("../../helpers");
+
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const result = await books.getAll();
-  res.json(result);
+  try {
+    const result = await books.getAll();
+    res.json(result);
+  } catch (error) {
+    next(error);
+    // res.status(500).json({ message: "Server error" });
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await books.getById(id);
+    if (!result) {
+      throw HttpError(404, "Not found");
+
+      // const error = new Error("Not found");
+      // error.status = 404;
+      // throw error;
+
+      // return res.status(404).json({ message: "Not found" });
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+    // const { status = 500, message = "server error" } = error;
+    // res.status(status).json({ message: message });
+  }
 });
 
 module.exports = router;
