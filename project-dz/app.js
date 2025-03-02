5c1cfe7ba2da83253dfdb244f07f79593c693159
Allovallo <grossco@meta.ua>
2025-03-02 10:47:18 +0200

    files updated

--- project-dz/app.js
+++ project-dz/app.js
@@ -14,7 +14,7 @@ app.use(cors());
 // app.use(express.json());
 
 app.use("/api/books", booksRouter);
-// app.use("/api/contacts", contactsRouter);
+app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
