a5362fbcc7b3b44381c58ce3c1d082743f8f8ab2
Allovallo <grossco@meta.ua>
2025-03-04 18:16:47 +0200

    files updated

--- project-dz/routes/api/books.js
+++ project-dz/routes/api/books.js
@@ -27,13 +27,13 @@ router.get("/:id", async (req, res, next) => {
     const { id } = req.params;
     const result = await books.getById(id);
     if (!result) {
-      throw HttpError(404, "Not found");
+      throw HttpError(404, "Not found!");
 
-      // const error = new Error("Not found");
+      // const error = new Error("Not found!");
       // error.status = 404;
       // throw error;
 
-      // return res.status(404).json({ message: "Not found" });
+      // return res.status(404).json({ message: "Not found!" });
     }
     res.json(result);
   } catch (error) {
@@ -52,6 +52,25 @@ router.post("/", async (req, res, next) => {
     }
     const result = await books.add(req.body);
     res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const { error } = addSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const { id } = req.params;
+    const result = await books.updateById(id, req.body);
+
+    if (!result) {
+      throw HttpError(404, "Not found!");
+    }
+
+    res.json(result);
   } catch (error) {
     next(error);
   }
