0ca1b848cdc045d43629049ec3597fdd0923243a
Allovallo <grossco@meta.ua>
2025-03-06 15:25:07 +0200

    files updated

--- controllers-decorates-maddleware-example/routes/api/books.js
+++ controllers-decorates-maddleware-example/routes/api/books.js
@@ -2,15 +2,19 @@ const express = require("express");
 
 const ctrl = require("../../controllers/books");
 
+const { validateBody } = require("../../middleware");
+
+const schemas = require("../../schemas/books");
+
 const router = express.Router();
 
 router.get("/", ctrl.getAll);
 
 router.get("/:id", ctrl.getById);
 
-router.post("/", ctrl.post);
+router.post("/", validateBody(schemas.addSchema), ctrl.add);
 
-router.put("/:id", ctrl.put);
+router.put("/:id", validateBody(schemas.addSchema), ctrl.updateById);
 
 router.delete("/:id", ctrl.deleteById);
 
