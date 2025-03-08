dd669680d76377127dd7687805988610f00ff3e4
Allovallo <grossco@meta.ua>
2025-03-08 17:48:30 +0200

    books.js updated

--- project-dz-2/routes/api/books.js
+++ project-dz-2/routes/api/books.js
@@ -5,8 +5,22 @@ const books = require("../../models/books");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const result = await books.getAll();
-  res.json(result);
+  try {
+    const result = await books.getAll();
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({ message: "SERVER ERRORR!!!" });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await books.getById(id);
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({ message: "SERVER ERRORR!!!" });
+  }
 });
 
 module.exports = router;
