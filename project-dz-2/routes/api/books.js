6ef38a3a99f994d72299dc4aebae4716918c2974
Allovallo <grossco@meta.ua>
2025-03-11 18:27:56 +0200

    Files updated

--- project-dz-2/routes/api/books.js
+++ project-dz-2/routes/api/books.js
@@ -45,6 +45,28 @@ router.post("/", async (req, res, next) => {
     if (error) {
       throw HttpError(400, error.message);
     }
+    const result = await books.add(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const { error } = addSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+
+    const { id } = req.params;
+    const result = await books.updateById(id, req.body);
+
+    if (!result) {
+      throw HttpError(404, "Not found!!!");
+    }
+
+    res.json(result);
   } catch (error) {
     next(error);
   }
