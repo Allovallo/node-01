758d96511bbb53d08e91019fecb18f7aa9563bc5
Allovallo <grossco@meta.ua>
2025-03-12 16:32:05 +0200

    files updated

--- project-dz-2/controllers/books.js
+++ project-dz-2/controllers/books.js
@@ -2,83 +2,69 @@ const Joi = require("joi");
 
 const books = require("../models/books");
 
-const { HttpError } = require("../helpers");
+const { HttpError, ctrlWrapper } = require("../helpers");
 
 const addSchema = Joi.object({
   title: Joi.string().required(),
   author: Joi.string().required(),
 });
 
-const getAll = async (req, res) => {
-  try {
-    const result = await books.getAll();
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
+const getAll = async (_, res) => {
+  const result = await books.getAll();
+  res.json(result);
 };
 
-const getById = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await books.getById(id);
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const result = await books.getById(id);
 
-    if (!result) {
-      throw HttpError(404, "NOT FOUND");
-    }
-
-    res.json(result);
-  } catch (error) {
-    next(error);
+  if (!result) {
+    throw HttpError(404, "NOT FOUND");
   }
+
+  res.json(result);
 };
 
-const add = async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const result = await books.add(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
+const add = async (req, res) => {
+  const { error } = addSchema.validate(req.body);
+  if (error) {
+    throw HttpError(400, error.message);
   }
+  const result = await books.add(req.body);
+  res.status(201).json(result);
 };
 
-const updatedById = async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-
-    const { id } = req.params;
-    const result = await books.updateById(id, req.body);
-
-    if (!result) {
-      throw HttpError(404, "Not found!!!");
-    }
-
-    res.json(result);
-  } catch (error) {
-    next(error);
+const updateById = async (req, res) => {
+  const { error } = addSchema.validate(req.body);
+  if (error) {
+    throw HttpError(400, error.message);
   }
+
+  const { id } = req.params;
+  const result = await books.updateById(id, req.body);
+
+  if (!result) {
+    throw HttpError(404, "Not found!!!");
+  }
+
+  res.json(result);
 };
 
-const deleteById = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await books.deleteById(id);
+const deleteById = async (req, res) => {
+  const { id } = req.params;
+  const result = await books.deleteById(id);
 
-    if (!result) {
-      throw HttpError(404, "Not found!!!");
-    }
-
-    res.json({ message: "Delete success!" });
-  } catch (error) {
-    next(error);
+  if (!result) {
+    throw HttpError(404, "Not found!!!");
   }
+
+  res.json({ message: "Delete success!" });
 };
 
-module.exports = { getAll, getById, add, updatedById, deleteById };
+module.exports = {
+  getAll: ctrlWrapper(getAll),
+  getById: ctrlWrapper(getById),
+  add: ctrlWrapper(add),
+  updateById: ctrlWrapper(updateById),
+  deleteById: ctrlWrapper(deleteById),
+};
--- project-dz-2/routes/api/books.js
+++ project-dz-2/routes/api/books.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get("/", ctrl.getAll);
 router.get("/:id", ctrl.getById);
 router.post("/", ctrl.add);
-router.put("/:id", ctrl.updatedById);
+router.put("/:id", ctrl.updateById);
 router.delete("/:id", ctrl.deleteById);
 
 module.exports = router;
