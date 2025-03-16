e930fddf060f2fe2cef2f53d69cf6cc71c615e42
Allovallo <grossco@meta.ua>
2025-03-16 12:02:33 +0200

    files updated

--- project-dz-3/routes/api/contacts.js
+++ project-dz-3/routes/api/contacts.js
@@ -46,4 +46,36 @@ router.post("/", async (req, res, next) => {
   }
 });
 
+router.put("/:id", async (req, res, next) => {
+  try {
+    const { error } = addSchema.validate(req.body);
+    if (error) {
+      throw HttpError(404, error.message);
+    }
+    const { id } = req.params;
+    const result = await contacts.updateById(id, req.body);
+
+    if (!result) {
+      throw HttpError(404, "Not found!");
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contacts.deleteById(id);
+
+    if (!result) {
+      throw HttpError(404, "Not found!");
+    }
+    res.json({ message: "Delete success!!!" });
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
