ed64dfc22f81451e714ef428dfa5e7a8c82ce0c3
grossco <30566134+Allovallo@users.noreply.github.com>
2025-10-12 19:56:33 +0300

    files updated

--- project-1/work-with-files/app.js
+++ project-1/work-with-files/app.js
@@ -1,38 +1,17 @@
 const fs = require("fs/promises");
-// const fs = require("fs").promises
-
-// const replaceText = async () => {
-//   const result = await fs.writeFile("./files/file.txt", "Гегель");
-//   console.log(result);
-// };
-
-// replaceText();
-
-const addText = async () => {
-  const result = await fs.appendFile("./files/file.txt", "./files/file1.txt");
-  console.log(result);
-};
-
-addText();
-
-/*
-const readFile = async () => {
-  const text = await fs.readFile("./files/file.txt", "utf-8");
-  //   const buffer = await fs.readFile("./files/file.txt");
-  //   console.log(buffer);
-  //   const text = buffer.toString();
-  console.log(text);
-};
-
-readFile();
-*/
-// fs.readFile("./files/file.txt")
-//   .then((data) => console.log(data))
-//   .catch((error) => console.log(error.message));
-
-// const fs = require("fs");
 
 // fs.readFile("./files/file.txt", (error, data) => {
 //   console.log(error);
 //   console.log(data);
 // });
+
+// fs.readFile("./files/file.txt")
+//   .then((data) => console.log(data))
+//   .catch((error) => console.log(error.message));
+
+const readFile = async () => {
+  const data = await fs.readFile("./files/file.txt");
+  console.log(data);
+};
+
+readFile();
