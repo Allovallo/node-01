3ede6e7ff9db2eed0139e53b89b7f56f4435bedf
grossco <30566134+Allovallo@users.noreply.github.com>
2025-10-19 13:01:05 +0300

    files updated

--- guess-the-number/index.js
+++ guess-the-number/index.js
@@ -1,15 +1,13 @@
 const readline = require("readline");
 const fs = require("fs/promises");
-require("colors");
-
 const { program } = require("commander");
+require("colors");
 
 program.option(
   "-f, --file [type]",
   "file for saving game results",
   "results.txt"
 );
-
 program.parse(process.argv);
 
 const rl = readline.createInterface({
@@ -18,16 +16,18 @@ const rl = readline.createInterface({
 });
 
 let count = 0;
+
 const logFile = program.opts().file;
+
 const mind = Math.floor(Math.random() * 10) + 1;
 
 const isValid = (value) => {
   if (isNaN(value)) {
-    console.log("Число має бути в діапазоні від 1 до 10!".red);
+    console.log("Введіть число!".red);
     return false;
   }
   if (value < 1 || value > 10) {
-    console.log("Число має бути в діапазоні від 1 до 10!".red);
+    console.log("Введіть число від 1 до 10!".red);
     return false;
   }
   return true;
@@ -36,7 +36,7 @@ const isValid = (value) => {
 const log = async (data) => {
   try {
     await fs.appendFile(logFile, `${data}\n`);
-    console.log(`Вдалося зберегти результат у файл ${logFile}`.green);
+    console.log(`Вдалося зберегти результат в файл ${logFile}`.green);
   } catch (error) {
     console.log(`Не вдалося зберегти файл ${logFile}`.red);
   }
@@ -48,6 +48,7 @@ const game = () => {
 
     (value) => {
       let a = +value;
+
       if (!isValid(a)) {
         game();
         return;
@@ -56,14 +57,19 @@ const game = () => {
       count += 1;
 
       if (a === mind) {
-        console.log("Вітаю, ви вгадали число за %d крок(ів)!".green, count);
+        console.log(
+          `Вітаю, ви вгадали число за ${count} крок(ів)`.green
+        );
 
         log(
-          `${new Date().toLocaleDateString()}: Вітаю, ви вгадали число за ${count} крок(ів)!`
+          `${new Date().toLocaleDateString()}: Вітаю, ви вгадали число за ${count} крок(ів)`
         ).finally(() => rl.close());
+
         return;
       }
+
       console.log("Ви не вгадали, ще спроба".red);
+
       game();
     }
   );
