3232b600bf1ac935b80973b004279fa6b61e56ce
grossco <30566134+Allovallo@users.noreply.github.com>
2025-10-23 18:05:51 +0300

    files updated

--- project-3/express-example-3/app.js
+++ project-3/express-example-3/app.js
@@ -1,8 +1,27 @@
 const express = require("express");
+const moment = require("moment");
+const fs = require("fs/promises");
 
 const books = require("./books");
 
 const app = express();
+
+// app.use((req, res, next) => {
+//   console.log("First middleware");
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   console.log("Second middleware");
+//   next();
+// });
+
+app.use(async (req, res, next) => {
+  const { method, url } = req;
+  const date = moment().format("DD-MM-YYYY_hh:mm:ss");
+  await fs.appendFile("./public/server.log", `\n${method} ${url} ${date}`);
+  next();
+});
 
 app.get("/products", (req, res) => {
   res.json([]);
